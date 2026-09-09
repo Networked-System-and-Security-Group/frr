8f83af13488666a4f5c6f2572c00772f2dc441ee
zhc <zhang-hc22@mails.tsinghua.edu.cn>
2026-09-09 08:17:21 +0000

    bgpd: remove unused MIDR control state

--- bgpd/bgp_midr_ctrl.c
+++ bgpd/bgp_midr_ctrl.c
@@ -1032,7 +1032,6 @@ static struct stream *midr_ctrl_build_member_list(struct bgp *bgp,
 						  struct ipaddr dst,
 						  uint32_t group_id)
 {
-	struct bgp_midr_nds *mi = bgp->midr_nds_info;
 	struct stream *s;
 	struct list *members;
 	struct listnode *node;
